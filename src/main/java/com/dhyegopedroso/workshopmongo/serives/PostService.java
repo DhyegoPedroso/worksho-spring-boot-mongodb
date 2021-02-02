aa3d4a6e67bc3b0b313bac341b5f640956adcf16
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-02-02 20:37:07 -0300

    Obtendo um post por id

---
+++ src/main/java/com/dhyegopedroso/workshopmongo/serives/PostService.java
@@ -0,0 +1,23 @@
+package com.dhyegopedroso.workshopmongo.serives;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dhyegopedroso.workshopmongo.domain.Post;
+import com.dhyegopedroso.workshopmongo.repository.PostRepository;
+import com.dhyegopedroso.workshopmongo.serives.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
